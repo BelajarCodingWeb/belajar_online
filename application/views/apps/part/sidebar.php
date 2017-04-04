a331c46c346aec717759f3bd61560014e23a69fa
AnnaHasyim <annahasyim97@gmail.com>
2017-04-04 11:21:12 +0700

    Angry Birds

---
+++ application/views/apps/part/sidebar.php
@@ -0,0 +1,22 @@
+<div class="container">
+<div class="row">
+<div class="col-md-2">
+
+<!-- uncomment code for absolute positioning tweek see top comment in css -->
+<div class="absolute-wrapper"> </div>
+<!-- Menu -->
+<div class="side-menu">
+<nav class="navbar navbar-default" role="navigation">
+<!-- Main Menu -->
+<div class="">
+  <ul class="nav nav-sidebar">
+    <li class="active"><a href="#"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a></li>
+    <li><a href="#"><span class="glyphicon glyphicon-plane"></span> Sticker</a></li>
+    <li><a href="#"><span class="glyphicon glyphicon-cloud"></span> Page</a></li>
+    <li><a href="#"><span class="glyphicon glyphicon-signal"></span> Setting Sistem</a></li>
+  </ul>
+</div><!-- /.navbar-collapse -->
+</nav>
+
+</div>
+</div>
