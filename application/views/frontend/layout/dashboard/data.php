79d33d25d8d4bd3ab484b7f476605b1311791954
AnnaHasyim <annahasyim97@gmail.com>
2017-04-06 15:17:30 +0700

    edit

--- application/views/frontend/layout/dashboard/data.php
+++ application/views/frontend/layout/dashboard/data.php
@@ -10,8 +10,8 @@
         <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
         <p>To see the difference between static and fixed top navbars, just scroll.</p>
         <p>
-          <a style="border-radius:5px" class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Create Free Account</a>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Explore Courses</a>
+          <a style="border-radius:50px" class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Create Free Account</a>
+          <a style="border-radius:50px" class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Explore Courses</a>
         </p>
       </div>
       </div>
@@ -130,3 +130,56 @@
         </div>
       </div>
     </div>
+
+      <div class="container">
+              <div class="row">
+                <div style="text-align:center;" class="col-md-6 col-md-offset-3">
+                <h2>Join the Crowd</h2>
+                <p >
+                  Code School has impacted over a million students from 237 countries. 
+                  Read how these students learn by doing with Code School courses and screencasts.
+                  </p>
+        </div>
+      </div>
+
+<div class="container" style="padding-top:60px">
+  <div class="row">
+    <div class="col-md-4">
+      <p style="font-size:14px">
+      “I had never tried to learn code before out of fear that I wasn’t ‘techy enough’ and probably not smart enough — I was a Mathlete, but I spent most of my time making doodles with the DRAW function of my TI89. Every time I finish a Code School lesson, I feel like I prove that fear wrong. I truly appreciate you making an accessible resource.”</p>
+      <div class="row">
+      <div class="col-md-4">
+        <img class="img-circle" style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+      </div>
+      <div class="col-md-8" style="padding-top:35px">
+        hjkgdelwjcjikjkln
+      </div>
+      </div>
+    </div>
+        <div class="col-md-4">
+      <p style="font-size:14px">
+      “I had never tried to learn code before out of fear that I wasn’t ‘techy enough’ and probably not smart enough — I was a Mathlete, but I spent most of my time making doodles with the DRAW function of my TI89. Every time I finish a Code School lesson, I feel like I prove that fear wrong. I truly appreciate you making an accessible resource.”</p>
+            <div class="row">
+      <div class="col-md-4">
+        <img class="img-circle" style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+      </div>
+      <div class="col-md-8" style="padding-top:35px">
+        hjkgdelwjcjikjkln
+      </div>
+      </div>
+    </div>
+        <div class="col-md-4">
+      <p style="font-size:14px">
+      “I had never tried to learn code before out of fear that I wasn’t ‘techy enough’ and probably not smart enough — I was a Mathlete, but I spent most of my time making doodles with the DRAW function of my TI89. Every time I finish a Code School lesson, I feel like I prove that fear wrong. I truly appreciate you making an accessible resource.”</p>
+            <div class="row">
+      <div class="col-md-4">
+        <img class="img-circle" style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+      </div>
+      <div class="col-md-8" style="padding-top:35px">
+        hjkgdelwjcjikjkln
+      </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
