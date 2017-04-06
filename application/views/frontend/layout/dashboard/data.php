055e4fb4f4306c752df8e47c15a086e5e6ba7fb3
AnnaHasyim <annahasyim97@gmail.com>
2017-04-06 14:21:03 +0700

    Edit Data

--- application/views/frontend/layout/dashboard/data.php
+++ application/views/frontend/layout/dashboard/data.php
@@ -1,38 +1,130 @@
 <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron" style="text-align: center;">
-        <h1>Navbar example</h1>
+      <div class="jumbotron" style="padding-top:50px; background-image:url(http://localhost/belajar_online/resources/images/back.jpg);
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 400px;">
+        <div style="text-align:center; color:#9e2222">
+        <h1><strong>Belajar Online</strong></h1>
         <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
         <p>To see the difference between static and fixed top navbars, just scroll.</p>
         <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+          <a style="border-radius:5px" class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Create Free Account</a>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Explore Courses</a>
         </p>
       </div>
-
-      <div  style="text-align: center; padding-bottom: 50px">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
+      </div>
       </div>
 
-      <div class="container">
+      <div class="container" style="text-align:center">
+        <h2>Learning Paths</h2>
+        <p>Code School courses are organized into Paths based on technology.Navigate to find the right course for you.</p>
+      </div>
+      
+    <div class="container" style="padding-top:50px">
       <div class="row">
       	<div class="col-md-6">
       		<div class="panel panel-default">
              <div class="panel-body">
-              Basic panel example
+              <div class="row">
+                <div class="col-md-4">
+                  <img style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+                </div>
+                <div class="col-md-8">
+                  gdvhjbsnyuhjegj
+                </div>
+              </div>
            </div>
          </div>
-      	</div>
+      </div>
       	<!-- di bagi 2 kolom -->
       	<div class="col-md-6">
       		<div class="panel panel-default">
              <div class="panel-body">
-              Basic panel example
+             <div class="row">
+                <div class="col-md-4">
+                  <img style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+                </div>
+                <div class="col-md-8">
+                  gdvhjbsnyuhjegj
+                </div>
+              </div>
            </div>
          </div>
-      	</div>
       </div>
+        <div class="col-md-6">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-4">
+                  <img style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+                  </div>
+                  <div class="col-md-8">
+                  gdvhjbsnyuhjegj
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+        <div class="col-md-6">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-4">
+                  <img style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+                  </div>
+                  <div class="col-md-8">
+                  gdvhjbsnyuhjegj
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        <div class="col-md-6">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-4">
+                  <img style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+                  </div>
+                  <div class="col-md-8">
+                  gdvhjbsnyuhjegj
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        <div class="col-md-6">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-4">
+                  <img style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+                  </div>
+                  <div class="col-md-8">
+                  gdvhjbsnyuhjegj
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>
+          </div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                  <div class="panel panel-default">
+                    <div class="panel-body">
+                    <div class="row">
+                    <div class="col-md-4">
+                    <img style="height:100px; width:auto" src="<?php echo base_url()?>resources/images/avatar/img_avatar2.png">
+                    </div>
+                    <div class="col-md-8">
+                    gdvhjbsnyuhjegj
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
--- application/views/frontend/part/header.php
+++ application/views/frontend/part/header.php
@@ -1,5 +1,5 @@
 <head>
-    <meta charset="utf-8">
+<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
@@ -14,7 +14,7 @@
   </head>
 
    <!-- Static navbar -->
-    <nav class="navbar navbar-default navbar-static-top">
+    <nav style="padding-top:5px; box-shadow: 0px 5px 20px #f2f7fd; height:10%" class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -26,27 +26,16 @@
           <a class="navbar-brand" href="#">Project name</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+          <ul class="nav nav-pills">
+            <li class="active"><a class="btn btn-success" href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
+          
+          <ul class="nav nav-pills pull-right">
             <li><a href="../navbar/">Default</a></li>
             <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
             <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+          </ul>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
