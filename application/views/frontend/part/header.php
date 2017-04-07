b72ab0b70f520b7ed99d44eee97e1c4209ca84f5
unknown <myazidinniam@gmail.com>
2017-04-07 10:50:53 +0700

    layout frontend

--- application/views/frontend/part/header.php
+++ application/views/frontend/part/header.php
@@ -22,8 +22,8 @@
     </script>
   </head>
    <!-- Static navbar -->
+   <div class="container">
     <nav class="navbar navbar-default">
-  <div class="">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="row">
       <div class="col-md-4" style="padding-top: 15px">
@@ -47,5 +47,5 @@
     </div><!-- /.navbar-collapse -->
       </div>
     </div>
-    </div>
 </nav>
+</div>
